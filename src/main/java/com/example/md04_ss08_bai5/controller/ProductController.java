c525d7d6566f5e959cffb14d9334d6e244eb3c33
PMT <tu.pm209@gmail.com>
2026-03-05 21:58:29 +0900

    a

--- src/main/java/com/example/md04_ss08_bai5/controller/ProductController.java
+++ src/main/java/com/example/md04_ss08_bai5/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.example.md04_ss08_bai5.controller;
 
 import com.example.md04_ss08_bai5.entity.Product;
+import com.example.md04_ss08_bai5.exception.GlobalExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -31,15 +34,19 @@ public class ProductController {
         }
 
         try {
-            String fileName = image.getOriginalFilename();
-            if(fileName == null || fileName.isEmpty()){
-                return new ResponseEntity<>("Ảnh sản phẩm không được để trống", HttpStatus.BAD_REQUEST);
-            }
+//
+//            if(fileName == null || fileName.isEmpty()){
+//                return new ResponseEntity<>("Ảnh sản phẩm không được để trống", HttpStatus.BAD_REQUEST);
+//            }
+//
+//            String lowercaseFileName = fileName.toLowerCase();
 
-            String lowercaseFileName = fileName.toLowerCase();
-            if(lowercaseFileName.endsWith(".png") || lowercaseFileName.endsWith(".jpg")){
-                String extension = lowercaseFileName.substring((lowercaseFileName.lastIndexOf(".")));
-                String newName = UUID.randomUUID() + extension;
+            String contentType = image.getContentType();
+            if(contentType == null || (!contentType.equals("image/jpeg") && !contentType.equals("image/png"))){
+                return new ResponseEntity<>("Chỉ nhận file ảnh jpeg/png", HttpStatus.BAD_REQUEST);
+            }else{
+                String extension = StringUtils.getFilenameExtension(image.getOriginalFilename());
+                String newName = UUID.randomUUID() + "." + extension;
 
                 Path uploadPath = Paths.get(UPLOAD_DIR);
                 if(!Files.exists(uploadPath)){
@@ -50,14 +57,16 @@ public class ProductController {
                 Files.copy(image.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
                 products.add(new Product(name, price, newName));
+
                 return new ResponseEntity<>("Upload sản phẩm thành công", HttpStatus.OK);
-            }else{
-                return new ResponseEntity<>("Chỉ nhận file ảnh jpg/png", HttpStatus.BAD_REQUEST);
+
             }
         } catch (IOException e) {
             e.printStackTrace();
             return new ResponseEntity<>("Lỗi server", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+
     }
 
     @GetMapping
